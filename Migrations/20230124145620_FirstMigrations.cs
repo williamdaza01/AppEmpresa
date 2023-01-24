195dd0fecf72110ea9e6e5a53876859b44597e01
Javier Lopez <jlopez@impactupgrade.com>
2023-01-24 09:57:05 -0500

    Agregar archivos de proyecto.

---
+++ Migrations/20230124145620_FirstMigrations.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AppEmpresa.Migrations
+{
+    /// <inheritdoc />
+    public partial class FirstMigrations : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Product",
+                columns: table => new
+                {
+                    productId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    productName = table.Column<string>(type: "nvarchar(100)", nullable: true),
+                    productDescription = table.Column<string>(type: "nvarchar(250)", nullable: true),
+                    productDateIn = table.Column<string>(type: "nvarchar(10)", nullable: true),
+                    productDateOut = table.Column<string>(type: "nvarchar(10)", nullable: true),
+                    productPrice = table.Column<int>(type: "int", nullable: true),
+                    productQuantity = table.Column<int>(type: "int", nullable: true),
+                    productIsFaulty = table.Column<bool>(type: "bit", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Product", x => x.productId);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Product");
+        }
+    }
+}
---
+++ Models/Product.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace AppEmpresa.Models
+{
+    public class Product
+    {
+        [Key]
+        public int productId { get; set; }
+
+        [Column(TypeName = "nvarchar(100)")]
+        public string? productName { get; set; }
+
+        [Column(TypeName = "nvarchar(250)")]
+        public string? productDescription { get; set; }
+
+        //mm/dd/aaaa
+        [Column(TypeName = "nvarchar(10)")]
+        public string? productDateIn { get; set; }
+
+        [Column(TypeName = "nvarchar(10)")]
+        public string? productDateOut { get; set; }
+
+        public int? productPrice { get; set; }
+        public int? productQuantity { get; set; }
+        public bool? productIsFaulty { get; set; }
+    }
+}
