6d7a08235c0f1e2b896902584c877474a9c615c3
William Daza <williamdaza04@gmail.com>
2023-01-24 14:59:07 -0500

    create model

---
+++ FrontAppEmpresa/src/app/shared/product.model.ts
@@ -0,0 +1,10 @@
+export class Product {
+    productId: number = 0;
+    productName: string = "";
+    productDescription: string = "";
+    productDateIn: string = "";
+    productDateOut: string = "";
+    productPrice: number = 0;
+    productQuantity: number = 0;
+    productIsFaulty: boolean = false;
+}
