2237feeb4ad6d84e19f08fb438f1c542cea84abc
William Daza <williamdaza04@gmail.com>
2023-01-24 14:58:56 -0500

    create first view

--- FrontAppEmpresa/src/app/components/product/product.component.ts
+++ FrontAppEmpresa/src/app/components/product/product.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { APIProductServiceService } from 'src/app/services/apiproduct-service.service';
 
 @Component({
   selector: 'app-product',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductComponent implements OnInit {
 
-  constructor() { }
+  constructor(public service:APIProductServiceService) { }
 
   ngOnInit(): void {
   }
