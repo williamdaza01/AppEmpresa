e6883ef9e36b4a85970c63097a712f7cb53d9381
William Daza <williamdaza04@gmail.com>
2023-01-24 14:59:24 -0500

    add dependencies

--- FrontAppEmpresa/src/app/app.module.ts
+++ FrontAppEmpresa/src/app/app.module.ts
@@ -1,18 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { ProductComponent } from './components/product/product.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { FormProductComponent } from './components/product/form-product/form-product.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProductComponent
+    ProductComponent,
+    FormProductComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    NgbModule
   ],
-  providers: [],
+  providers: [HttpClientModule],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
